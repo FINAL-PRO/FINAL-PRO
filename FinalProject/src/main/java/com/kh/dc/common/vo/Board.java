870a767e0b4a0aedd42594a59b742833ada51a91
eo7655 <eo7655@naver.com>
2018-12-18 12:28:28 +0900

    board add

---
+++ FinalProject/src/main/java/com/kh/dc/common/vo/Board.java
@@ -0,0 +1,101 @@
+package com.kh.dc.common.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Board implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7132581284642993330L;
+	
+	private int no;
+	private String title;
+	private Date writeDate;
+	private String content;
+	private int viewCount;
+	private String type;
+	private String status;
+	private int memberNo;
+	private String category;
+	
+	public Board() {
+		super();
+	}
+	
+	public Board(int no, String title, Date writeDate, String content, int viewCount, String type, String status,
+			int memberNo, String category) {
+		super();
+		this.no = no;
+		this.title = title;
+		this.writeDate = writeDate;
+		this.content = content;
+		this.viewCount = viewCount;
+		this.type = type;
+		this.status = status;
+		this.memberNo = memberNo;
+		this.category = category;
+	}
+
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [no=" + no + ", title=" + title + ", writeDate=" + writeDate + ", content=" + content
+				+ ", viewCount=" + viewCount + ", type=" + type + ", status=" + status + ", memberNo=" + memberNo
+				+ ", category=" + category + "]";
+	}
+}
