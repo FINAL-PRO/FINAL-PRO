da7e829b1525c7df435a2ead9866e6d99aac0a42
eo7655 <eo7655@naver.com>
2018-12-20 15:01:23 +0900

    어드민 보드 리스트 > 게시글타입별로 분리

--- FinalProject/src/main/java/com/kh/dc/admin/board/model/service/AdminBoardService.java
+++ FinalProject/src/main/java/com/kh/dc/admin/board/model/service/AdminBoardService.java
@@ -3,9 +3,12 @@ package com.kh.dc.admin.board.model.service;
 import java.util.List;
 
 import com.kh.dc.common.vo.Board;
+import com.kh.dc.common.vo.Code;
 
 public interface AdminBoardService {
-	public List<Board> selectBoardList();
+	public List<Board> selectBoardList(String boardType);
 
 	public Board selectBoardOne(int boardNo);
+
+	public List<Code> selectBoardTypeList();
 }
