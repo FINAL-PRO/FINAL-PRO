27037bdb4ca09b3624ec4164836f08bbf3759ef8
julys9 <julys9@naver.com>
2019-01-04 09:08:14 +0900

    commit

--- FinalProject/src/main/java/com/kh/dc/like/controller/LikeController.java
+++ FinalProject/src/main/java/com/kh/dc/like/controller/LikeController.java
@@ -1,12 +1,10 @@
 package com.kh.dc.like.controller;
 
-import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
