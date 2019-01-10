750a4ae7dc3cd8109e32976a4b8dc5fff7cdc301
kimsora <kimsorwa@naver.com>
2019-01-10 15:14:12 +0900

    190110 sora commit5

--- FinalProject/src/main/java/com/kh/dc/search/controller/SearchController.java
+++ FinalProject/src/main/java/com/kh/dc/search/controller/SearchController.java
@@ -1,6 +1,8 @@
 package com.kh.dc.search.controller;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,11 +25,30 @@ public class SearchController {
 	public String memberEnroll(@RequestParam String searchWord, Model model) {
 		
 		List<Board> searchResultList = searchService.searchResultList(searchWord);
-	
+							
+		for(int i = 0; i < searchResultList.size(); i++) {
+			String regex = "<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>";
+			String str = searchResultList.get(i).getContent();
+			String temp = "";
+
+			Pattern pattern = Pattern.compile(regex);
+
+			Matcher matcher = pattern.matcher(str);
+
+			while (matcher.find()) {
+
+			temp = matcher.replaceAll("");
+
+			
+			}	
+			System.out.println("temp : " + temp);
+			searchResultList.get(i).setContent(temp);		
+		
+		}		
+		
 		model.addAttribute("searchResultList", searchResultList);
 		
 		return "/search/searchView";
-		
-	}		
+	}
 
 }
